5a5c3cbd98e2e4ec0505ae918ca8f91e118c7b6b
Vadim <vad.general@mail.ru>
2023-03-23 23:28:41 +0300

    Build: изменить зависимости, удалить лишние файлы

--- src/test/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users(id, login, password, is_online) VALUES (4, 'correct1@mail.ru', 'sdkcfbk32%as.', false),
-                                                         (9, 'correct2@google.com', 'sdkcsdffbk32%as.', false),
-                                                         (233, 'correct3@yandex.ru', 'sdkc235fbk32%as.', false);
