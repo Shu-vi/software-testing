ee1b7fce175e5d9adc029667bed52ff9fbade7c5
Vadim <vad.general@mail.ru>
2023-04-19 13:19:54 +0300

    Feat: добавить кастомные тапы по координатам и скролл через драйвер

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,12 +1,8 @@
-package test;
-
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
-import org.openqa.selenium.Dimension;
 
 import java.time.Duration;
 
