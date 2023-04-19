38a2da8e202e3b0ad6e88457d143fd223b0b6f10
Vadim <vad.general@mail.ru>
2023-04-20 00:28:42 +0300

    Build: убрать неиспользуемую зависимость

---
+++ src/test/java/AuthPage.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selenide.$;
+
+public class NewsFeedPage {
+    private SelenideElement loginField = $(By.id("ru.ok.android:id/text_login"));
+    private SelenideElement passwordField = $(By.id("ru.ok.android:id/password_text"));
+    private SelenideElement loginButton = $(By.id("ru.ok.android:id/login_button"));
+
+
+    public void setLoginField(String loginField) {
+        this.loginField.should(exist).setValue(loginField);
+    }
+
+    public void setPasswordField(String passwordField) {
+        this.passwordField.should(exist).setValue(passwordField);
+    }
+
+    public void loginButtonClick() {
+        this.loginButton.should(exist).click();
+    }
+}
