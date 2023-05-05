61e8c156e66b87719d1f5fec911c8237e586a307
Vadim <vad.general@mail.ru>
2023-05-05 23:26:23 +0300

    Feat: добавить вспомогательные классы

--- src/test/java/JSONService.java
+++ src/test/java/JSONService.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class JSONService {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class JSONService {
+
+    public static JSONObject readJsonFromFile(String path) {
+        File file = new File(path);
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(rd);
+            return new JSONObject(jsonText);
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static String getUrl(JSONObject jsonObject) throws JSONException {
+        return jsonObject.getJSONObject("appium").getString("url");
+    }
+
+    public static JSONObject getCapabilities(JSONObject jsonObject) {
+        try {
+            return jsonObject.getJSONObject("appium").getJSONObject("capabilities");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -7,7 +7,7 @@ import io.appium.java_client.touch.offset.PointOption;
 import java.time.Duration;
 
 public class Utils {
-    public static void swipe(int startX, int startY, int endY){
+    public static void swipe(int startX, int startY, int endY) {
         AndroidDriver driver = (AndroidDriver) WebDriverRunner.getWebDriver();
         new TouchAction(driver)
                 .press(PointOption.point(startX, startY))
@@ -31,7 +31,38 @@ public class Utils {
         }
     }
 
-    public static void tap(int x, int y){
+    public static void scrollToLeft(int count) {
+        AndroidDriver androidDriver = (AndroidDriver) WebDriverRunner.getWebDriver();
+        for (int i = 0; i < count; i++) {
+            androidDriver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollToBeginning(10)");
+        }
+    }
+
+    public static void scrollToRight(String selectorType, String selectorValue) {
+        AndroidDriver androidDriver = (AndroidDriver) WebDriverRunner.getWebDriver();
+        String uiAutomatorString = "";
+        switch (selectorType) {
+            case "id": {
+                uiAutomatorString = "new UiScrollable(new UiSelector().resourceId(\"" + selectorValue + "\")).setAsHorizontalList().scrollToEnd(10)";
+                break;
+            }
+            case "text": {
+                uiAutomatorString = "new UiScrollable(new UiSelector().text(\"" + selectorValue + "\")).setAsHorizontalList().scrollToEnd(10)";
+                break;
+            }
+            case "description":
+                uiAutomatorString = "new UiScrollable(new UiSelector().description(\"" + selectorValue + "\")).setAsHorizontalList().scrollToEnd(10)";
+                break;
+            case "className":
+                uiAutomatorString = "new UiScrollable(new UiSelector().className(\"" + selectorValue + "\")).setAsHorizontalList().scrollToEnd(10)";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid selector type: " + selectorType);
+        }
+        androidDriver.findElementByAndroidUIAutomator(uiAutomatorString);
+    }
+
+    public static void tap(int x, int y) {
         AndroidDriver driver = (AndroidDriver) WebDriverRunner.getWebDriver();
         new TouchAction(driver)
                 .tap(PointOption.point(x, y))
