05cd2a76737b47b87eaa16f3d4d26ce4dc9ec957
Vadim <vad.general@mail.ru>
2023-05-05 23:25:42 +0300

    Feat: добавить конфигурацию для тестов

--- src/test/java/Config.java
+++ src/test/java/Config.java
@@ -1,8 +1,8 @@
 import org.openqa.selenium.Platform;
 
-public class Configuration {
+public class Config {
 
     public static final int RETRY_COUNT = 3;
-    public static final Platform PLATFORM = Platform.IOS;
-    public static final int TIMEOUTS = 10; // in seconds
+    public static final Platform PLATFORM = Platform.ANDROID;
+    public static final int TIMEOUTS = 120000; // in milliseconds
 }
