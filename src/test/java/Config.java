cf42d2e9fa255de6009126d8b46c3cd1e67485b4
Vadim <vad.general@mail.ru>
2023-05-05 23:21:00 +0300

    Build: добавить testNG и json

---
+++ src/test/java/Config.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.Platform;
+
+public class Configuration {
+
+    public static final int RETRY_COUNT = 3;
+    public static final Platform PLATFORM = Platform.IOS;
+    public static final int TIMEOUTS = 10; // in seconds
+}
