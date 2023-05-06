219170461092b869a4086e77d7f7ef409fa06b92
Vadim <vad.general@mail.ru>
2023-05-07 01:50:14 +0300

    Test: добавить создание ios драйвера в зависимости от указанной платформы

--- src/test/java/DriverOK.java
+++ src/test/java/DriverOK.java
@@ -1,4 +1,7 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.json.JSONObject;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -6,12 +9,12 @@ import java.net.URL;
 
 
 public class DriverOK {
-    public static AndroidDriver createDriver() {
+    public static AppiumDriver<MobileElement> createDriver() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         switch (Config.PLATFORM) {
-//            case IOS -> {
-//                return setIOSCapabilities(capabilities);
-//            }
+            case IOS : {
+                return setIOSCapabilities(capabilities);
+            }
             case ANDROID: {
                 return setAndroidCapabilities(capabilities);
             }
@@ -20,28 +23,28 @@ public class DriverOK {
         }
     }
 
-//    private MyAndroidDriver setIOSCapabilities(DesiredCapabilities capabilities) {
-//        IOSDriver<MobileElement> driver = null;
-//        String path = System.getProperty("user.dir") + "\\src\\test\\resources\\ios.json";
-//        JSONObject appiumJson = JSONService.readJsonFromFile(path);
-//        JSONObject caps = JSONService.getCapabilities(appiumJson);
-//        caps.keySet().forEach(keyStr -> capabilities.setCapability(keyStr, caps.get(keyStr)));
-//        try {
-//            driver = new IOSDriver<>(new URL(JSONService.getUrl(appiumJson)), capabilities);
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//        return driver;
-//    }
+    private static IOSDriver<MobileElement> setIOSCapabilities(DesiredCapabilities capabilities) {
+        IOSDriver<MobileElement> driver = null;
+        String path = System.getProperty("user.dir") + "\\src\\test\\resources\\ios.json";
+        JSONObject appiumJson = JSONService.readJsonFromFile(path);
+        JSONObject caps = JSONService.getCapabilities(appiumJson);
+        caps.keySet().forEach(keyStr -> capabilities.setCapability(keyStr, caps.get(keyStr)));
+        try {
+            driver = new IOSDriver<>(new URL(JSONService.getUrl(appiumJson)), capabilities);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return driver;
+    }
 
-    private static AndroidDriver setAndroidCapabilities(DesiredCapabilities capabilities) {
-        AndroidDriver driver = null;
+    private static AndroidDriver<MobileElement> setAndroidCapabilities(DesiredCapabilities capabilities) {
+        AndroidDriver<MobileElement> driver = null;
         String path = System.getProperty("user.dir") + "\\src\\test\\resources\\pixel2.json";
         JSONObject appiumJson = JSONService.readJsonFromFile(path);
         JSONObject caps = JSONService.getCapabilities(appiumJson);
         caps.keySet().forEach(keyStr -> capabilities.setCapability(keyStr, caps.get(keyStr)));
         try {
-            driver = new AndroidDriver(new URL(JSONService.getUrl(appiumJson)), capabilities);
+            driver = new AndroidDriver<>(new URL(JSONService.getUrl(appiumJson)), capabilities);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
