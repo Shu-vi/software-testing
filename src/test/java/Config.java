29865c7936655ec3983fb47492dda7b9c96d2d02
Vadim <vad.general@mail.ru>
2023-05-07 12:31:37 +0300

    Test: удалить количество перезапуска тестов(количество задавать из build.gradle)

--- src/test/java/Config.java
+++ src/test/java/Config.java
@@ -1,8 +1,6 @@
 import org.openqa.selenium.Platform;
 
 public class Config {
-
-    public static final int RETRY_COUNT = 3;
     public static final Platform PLATFORM = Platform.ANDROID;
-    public static final int TIMEOUTS = 120000; // in milliseconds
+    public static final int TIMEOUTS = 30000; // in milliseconds
 }
